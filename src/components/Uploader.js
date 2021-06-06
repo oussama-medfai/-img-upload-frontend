fb1621b8b2fd4eee7c0deb836c58f0fb43ca50f0
oussama-medfai <oussama-medfai>
2021-06-06 12:33:23 +0100

    deploy 2

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,28 +1,29 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Upload, Icon, message } from 'antd';
+import React from "react";
+import styled from "styled-components";
+import { Upload, Icon, message } from "antd";
 const { Dragger } = Upload;
 
 const Container = styled.div`
     width: 100%;
     height: 100%;
-`
+`;
 
 const Uploader = () => {
-
     const props = {
-        name: 'photo',
+        name: "photo",
         multiple: false,
-        action: `${process.env.REACT_APP_BASE_URL}/photo'`,
+        action: `${process.env.REACT_APP_BASE_URL}/photo`,
         onChange(info) {
             const { status } = info.file;
-            if (status !== 'uploading') {
+            if (status !== "uploading") {
                 console.log(info.file, info.fileList);
             }
 
-            if (status === 'done') {
-                message.success(`${info.file.name} file uploaded successfully.`);
-            } else if (status === 'error') {
+            if (status === "done") {
+                message.success(
+                    `${info.file.name} file uploaded successfully.`
+                );
+            } else if (status === "error") {
                 message.error(`${info.file.name} file upload failed.`);
             }
         },
@@ -31,15 +32,17 @@ const Uploader = () => {
     return (
         <Container>
             <Dragger {...props}>
-                <div style={{width: '100%'}}>
+                <div style={{ width: "100%" }}>
                     <p className="ant-upload-drag-icon">
                         <Icon type="inbox" />
                     </p>
-                    <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                    <p className="ant-upload-text">
+                        Click or drag file to this area to upload
+                    </p>
                 </div>
             </Dragger>
         </Container>
-    )
-}
+    );
+};
 
 export default Uploader;
